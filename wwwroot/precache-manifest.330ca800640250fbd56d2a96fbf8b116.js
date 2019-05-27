98268fc660ff1748e482fc31de02106dd865b575
Nano Vazquez <marianodvazquez@gmail.com>
2019-05-26 22:48:25 -0300

    Add wwwroot files in repository

---
+++ wwwroot/precache-manifest.330ca800640250fbd56d2a96fbf8b116.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "eaf8e273bf484f86ba65c9779995f415",
+    "url": "/static/media/vopen-logo.eaf8e273.svg"
+  },
+  {
+    "revision": "4b5c923ec87f78e0adf725bd434cbbf2",
+    "url": "/static/media/social-icons.4b5c923e.svg"
+  },
+  {
+    "revision": "8d3460bb783e51cc621b3ae8dbcdc3d4",
+    "url": "/static/media/arrow-right.8d3460bb.svg"
+  },
+  {
+    "revision": "55db8822bb8c6844a0934830475eb5ab",
+    "url": "/static/media/arrow-bottom.55db8822.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "347e9025cb30b96a0d7a",
+    "url": "/static/js/main.70efdacc.chunk.js"
+  },
+  {
+    "revision": "e52e6080d23cf80565eb",
+    "url": "/static/js/2.b4d27f3a.chunk.js"
+  },
+  {
+    "revision": "347e9025cb30b96a0d7a",
+    "url": "/static/css/main.96d50fd1.chunk.css"
+  },
+  {
+    "revision": "78fef83ba2f47a1e19433f1654bd8c8c",
+    "url": "/index.html"
+  }
+];

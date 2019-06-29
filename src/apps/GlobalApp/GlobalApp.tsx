f8256b5da89c4f29085a9245fdc8bb6c7b13b8e1
Mariano Vazquez <marianodvazquez@gmail.com>
2019-06-29 20:01:34 -0300

    Add flags to Global conference site (#5)

--- src/apps/GlobalApp/GlobalApp.tsx
+++ src/apps/GlobalApp/GlobalApp.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Header, Footer, NavLink, PageSection, Banner, About, PastEditions } from "../../components";
+import { FlagArgentina, FlagChile, FlagColombia, FlagPeru, FlagUruguay } from "../../components/SVGs";
 import { Conduct, Sponsorship, Team } from "../../pages";
+
 import styles from "./GlobalApp.module.scss";
 
 const Home = () => {
   return (
     <div className={styles.home}>
-      <Banner to="#about" title="2019" />
+      <Banner to="#about" title="2019">
+        <div className={styles.flags}>
+          <FlagArgentina className={styles.flag} onClick={() => window.open("//ar.vopen.tech", "_blank")} />
+          <FlagChile className={styles.flag} onClick={() => window.open("//cl.vopen.tech", "_blank")} />
+          <FlagColombia className={styles.flag} onClick={() => window.open("//co.vopen.tech", "_blank")} />
+          <FlagPeru className={styles.flag} onClick={() => window.open("//pe.vopen.tech", "_blank")} />
+          <FlagUruguay className={styles.flag} onClick={() => window.open("//uy.vopen.tech", "_blank")} />
+        </div>
+      </Banner>
       <PageSection id="about" title="About">
         <About />
       </PageSection>

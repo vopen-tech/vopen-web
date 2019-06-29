f2ddc9b2feca45eb2fc323adfaf7f9970c463480
Nano Vazquez <marianodvazquez@gmail.com>
2019-06-29 19:49:01 -0300

    Add flags to Global conference site

--- src/components/SVGs/index.ts
+++ src/components/SVGs/index.ts
@@ -2,3 +2,9 @@ export { ReactComponent as ArrowBottom } from "./arrow-bottom.svg";
 export { ReactComponent as ArrowRight } from "./arrow-right.svg";
 export { ReactComponent as SocialIcons } from "./social-icons.svg";
 export { ReactComponent as VOpenLogo } from "./vopen-logo.svg";
+
+export { ReactComponent as FlagArgentina } from "./flags/flag-argentina.svg";
+export { ReactComponent as FlagChile } from "./flags/flag-chile.svg";
+export { ReactComponent as FlagColombia } from "./flags/flag-colombia.svg";
+export { ReactComponent as FlagPeru } from "./flags/flag-peru.svg";
+export { ReactComponent as FlagUruguay } from "./flags/flag-uruguay.svg";

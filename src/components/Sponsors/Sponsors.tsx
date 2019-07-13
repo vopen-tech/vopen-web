e5a40569bd863b70bffbf7085ad6879ddb595eed
Mariano Vazquez <marianodvazquez@gmail.com>
2019-07-13 11:32:19 -0300

    Add sponsors component (#8)

--- src/components/Sponsors/Sponsors.tsx
+++ src/components/Sponsors/Sponsors.tsx
@@ -1,24 +1,61 @@
 import React from "react";
 import classNames from "classnames";
-import { ActionButton, SponsorshipPackages } from "..";
-import constants from "../../constants";
 
+import { ISponsor } from "../../types/ISponsor";
 import { Props, State } from "./types";
 import styles from "./Sponsors.module.scss";
 
+const Sponsor = ({ sponsor }: { sponsor: ISponsor }) => {
+  const cssClass = classNames(styles.sponsor, styles[sponsor.type]);
+
+  return (
+    <a className={cssClass} href={`${sponsor.url}?ref=vopen`} target="_blank">
+      <img className={styles.sponsorImage} src={sponsor.imageUrl} />
+    </a>
+  );
+};
+
 export default class Sponsors extends React.PureComponent<Props, State> {
   static defaultProps: Partial<Props> = {
-    className: undefined
+    className: undefined,
+    title: "",
+    sponsors: []
   };
 
   render() {
-    const { className } = this.props;
+    const { className, title, sponsors } = this.props;
     const cssClasses = classNames(styles.sponsors, className);
+
+    if (!sponsors || !sponsors.length) {
+      return null;
+    }
+
+    const diamondSponsors = sponsors.filter(item => item.type === "diamond");
+    const goldSponsors = sponsors.filter(item => item.type === "gold");
+    const silverSponsors = sponsors.filter(item => item.type === "silver");
+    const digitalSponsors = sponsors.filter(item => item.type === "digital");
+    const otherSponsors = sponsors.filter(item => item.type !== "diamond" && item.type !== "gold" && item.type !== "silver" && item.type !== "digital");
+
+    const Sponsors = ({ items }: { items: ISponsor[] }) => (
+      <div className={styles.sponsorsRow}>
+        {items.map(item => (
+          <Sponsor key={item.id} sponsor={item} />
+        ))}
+      </div>
+    );
 
     return (
       <div className={cssClasses}>
-        <ActionButton type="tertiary" text="Quiero ser sponsor" url={constants.sponsorsCallUrl} />
-        <SponsorshipPackages type="odd" />
+        {title && (
+          <div className={styles.title}>
+            <h3>{title}</h3>
+          </div>
+        )}
+        <Sponsors items={diamondSponsors} />
+        <Sponsors items={goldSponsors} />
+        <Sponsors items={silverSponsors} />
+        <Sponsors items={digitalSponsors} />
+        <Sponsors items={otherSponsors} />
       </div>
     );
   }
---
+++ src/types/ISponsor.ts
@@ -0,0 +1,8 @@
+export interface ISponsor {
+  id: string;
+  name: string;
+  country: string;
+  imageUrl: string;
+  url: string;
+  type: string;
+}

4db3347fc1e570881414004b4b02c381816a79b7
Nano Vazquez <marianodvazquez@gmail.com>
2019-07-24 13:57:03 -0300

    Fix vopen global linkedin

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,7 +10,7 @@ export default class Footer extends React.PureComponent<any, any> {
     return (
       <footer className={styles.footer}>
         <SocialIcon iconType="twitter" url="https://twitter.com/vopentech" />
-        <SocialIcon iconType="linkedin" url="https://linkedin.com/vopentech" />
+        <SocialIcon iconType="linkedin" url="https://linkedin.com/company/vopentech" />
         <SocialIcon iconType="facebook" url="https://facebook.com/vopentech" />
         <SocialIcon iconType="instagram" url="https://instagram.com/vopentech" />
         <SocialIcon iconType="youtube" url="https://youtube.com/vopentech" />

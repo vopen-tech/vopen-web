d45d6845499ddd94f47c93bb9cf469495f48db40
Nano Vazquez <marianodvazquez@gmail.com>
2019-12-08 19:57:21 -0300

    Disable schedule section if not provided

--- src/apps/ConferenceApp/ConferenceApp.tsx
+++ src/apps/ConferenceApp/ConferenceApp.tsx
@@ -49,6 +49,7 @@ const Home: React.SFC<any> = ({ conferenceInfo, globalInfo }: { conferenceInfo: 
   const conferenceSpeakers = conferenceInfo.speakers || [];
   const conferenceSponsors = conferenceInfo.sponsors || [];
   const conferenceActivities = conferenceInfo.activities || {};
+  const isScheduleEnabled = conferenceActivities.days && conferenceActivities.days.length > 0;
 
   return (
     <>
@@ -76,9 +77,11 @@ const Home: React.SFC<any> = ({ conferenceInfo, globalInfo }: { conferenceInfo: 
           <Tickets tickets={conferenceInfo.editionTickets} />
         </PageSection>
       )}
-      <PageSection id="schedule" title={Resources.pages.schedule}>
-        <Schedule activities={conferenceActivities} />
-      </PageSection>
+      {isScheduleEnabled && (
+        <PageSection id="schedule" title={Resources.pages.schedule}>
+          <Schedule activities={conferenceActivities} />
+        </PageSection>
+      )}
       <PageSection id="team" title={Resources.pages.team} type="odd">
         <Team team={conferenceOrganizers} type="odd" />
         <h4 className={styles.centeredText} style={{ margin: "35px 0 50px 0" }}>

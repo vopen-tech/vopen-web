ea53238a4ffa53b5b58cb73d55d870513239f4d5
iamcgd <hola@iamcgd.com>
2020-02-26 14:28:26 -0300

    add banner to schedule

--- src/pages/SchedulePage/SchedulePage.tsx
+++ src/pages/SchedulePage/SchedulePage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Schedule, PageSection } from "../../components";
+import { resourcesService } from "../../services";
 
 import styles from "./SchedulePage.module.scss";
 import { Props } from "./types";
@@ -7,9 +8,13 @@ import { Props } from "./types";
 export default class SchedulePage extends React.PureComponent<Props> {
   render() {
     const { activities } = this.props;
+    const Resources = resourcesService.getResources();
     return (
       <>
         <PageSection className={styles.schedulePage}>
+        <div className={styles.banner}>
+          <h1 className={styles.subtitle}>{Resources.pages.schedule}</h1>
+        </div>
           <Schedule activities={activities} />
         </PageSection>
       </>

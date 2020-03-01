3be2600598d4fb6c0b420355bcb4827c894ae69c
iamcgd <hola@iamcgd.com>
2020-02-29 22:39:04 -0300

    fix hero

--- src/components/HeroConf/HeroConf.tsx
+++ src/components/HeroConf/HeroConf.tsx
@@ -21,37 +21,33 @@ export default class HeroConf extends React.PureComponent<Props, State> {
 
     return (
       <div className={cssClasses}>
-        <div className={styles.hero}>
-          <div className="flex flex-wrap">
-            <div className="w-100">
-              <div className={styles.info}>
-                <div className="self-center">
-                  <div className="flex flex-wrap items-center justify-centerself-center">
-                    <VOpenLogo className={styles.logo} />
-                    <h1 className={styles.tag}>{conferenceTitle}</h1>
-                  </div>
-                  <h2 className={styles.title}>{Resources.titles.homePage}</h2>
-                  <p className={styles.bajada}>{Resources.info.mission}</p>
+      <div className={styles.hero}>
+        <div className="flex flex-wrap">
+          <div className="w-100">
+            <div className={styles.info}>
+              <div className="self-center">
+                <div className="flex flex-wrap items-center justify-centerself-center">
+                  <VOpenLogo className={styles.logo} />
+                  <h1 className={styles.tag}>{conferenceTitle}</h1>
                 </div>
+                <h2 className={styles.title}>{Resources.titles.homePage}</h2>
+                <p className={styles.bajada}>{Resources.info.mission}</p>
               </div>
-              <div className={styles.textPast}>
-                <div className="flex flex-wrap">
-                  {isTicketSaleEnabled && <ActionButton type="primary" text={Resources.banner.ticketsTitle} url="/#tickets" target="_self" />}
-                </div>
-                <div className="flex flex-wrap">
-                  <div className={styles.textData}>
-                    <p className={styles.time}>{conferenceInfo.date}</p>
-                    <p>
-                      <a href="/#location">{conferenceInfo.locationName}</a>{" "}
-                    </p>
-                  </div>
+            </div>
+            <div className={styles.textActions}>         
+              <div>{isTicketSaleEnabled && <ActionButton type="primary" text={Resources.banner.ticketsTitle} url="/#tickets" target="_self" />} </div>
+              <div className="flex flex-wrap">
+                <div className={styles.textData}>
+                  <p className={styles.time}>{conferenceInfo.date}</p>
+                  <p><a href="/#location">{conferenceInfo.locationName}</a></p>
                 </div>
               </div>
             </div>
           </div>
-          {children}
         </div>
+        {children}
       </div>
+    </div>
     );
   }
 }

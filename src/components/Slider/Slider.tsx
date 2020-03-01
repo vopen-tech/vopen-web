6f3dbb1b72a56f5458ce9208d917e45a9b9c60f7
iamcgd <hola@iamcgd.com>
2020-03-01 19:39:35 -0300

    add Flag

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -5,6 +5,7 @@ import { Props, State } from "./types";
 import styles from "./Slider.module.scss";
 import { ActionButton } from "../../components";
 import constants from "../../constants";
+import { FlagArgentina, FlagChile, FlagMexico, FlagUruguay } from "../../components/SVGs";
 
 export default class Slider extends React.PureComponent<Props, State> {
   static defaultProps: Partial<Props> = {
@@ -25,7 +26,10 @@ export default class Slider extends React.PureComponent<Props, State> {
                     <h3>vOpen MX 2020 </h3>
                     <p>Tecnológico Nacional de México Campus Tlahuac</p>
                     <p className={styles.timeSlider}>26, 27 Y 28 DE MARZO</p>
-                    <p  className={styles.urlSlider}><a href="https://mx.vopen.tech" target="_blank" rel="noopener">{Resources.buttons.visit} mx.vopen.tech</a></p>
+                    <p className={styles.urlSlider}><a href="https://mx.vopen.tech" target="_blank" rel="noopener">{Resources.buttons.visit} mx.vopen.tech</a></p>
+                    <div className={styles.flags}>
+                      <FlagMexico className={styles.flag} onClick={() => window.open("//mx.vopen.tech", "_blank")} />
+                      </div>
                 </div>
             </div>
             <div className="w-60-l w-100 relative">

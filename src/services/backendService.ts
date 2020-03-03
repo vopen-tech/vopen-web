faf1acb18a7901502541938e77a526b4a63a1d13
Nano Vazquez <marianodvazquez@gmail.com>
2020-03-03 14:07:51 -0300

    Set language for backend API

--- src/services/backendService.ts
+++ src/services/backendService.ts
@@ -7,10 +7,15 @@ const apiBasePath = "https://api.vopen.tech/api";
 async function fetchConference(conferenceId: string): Promise<IEdition | undefined> {
   const url = `${apiBasePath}/v1/editions/${conferenceId}`;
   const activeSiteLanguage = siteService.getSiteLanguageAndRegion();
+
+  // For now, the backend only stores data either in es-AR or en-US
+  // if we have to regionalize the data, we'll have to update the API's code
+  const siteLanguage = activeSiteLanguage.startsWith("es-") ? "es-AR" : "en-US";
+
   const config: AxiosRequestConfig = {
     headers: {
       // See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Language
-      "Accept-Language": activeSiteLanguage || "*"
+      "Accept-Language": siteLanguage || "*"
     }
   };
 

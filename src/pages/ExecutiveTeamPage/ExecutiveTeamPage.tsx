05bf7cc47359f584a61cd066f6ee620c29fbaa57
Facundo La Rocca <facundo_larocca@yahoo.com.ar>
2020-08-19 13:16:44 -0300

    Removed commented code

--- src/pages/ExecutiveTeamPage/ExecutiveTeamPage.tsx
+++ src/pages/ExecutiveTeamPage/ExecutiveTeamPage.tsx
@@ -4,45 +4,6 @@ import { Props } from "./types";
 import { resourcesService } from "../../services";
 import styles from "./ExecutiveTeamPage.module.scss";
 
-// const team: IUser[] = [
-//   {
-//     id: "Fabian Imaz",
-//     name: "Fabian Imaz",
-//     imageUrl: "http://uy.netconf.global/Content/images/demo/organizers/FabianImaz.jpg",
-//     socialLinks: [
-//       { type: "Twitter", url: "https://twitter.com/fabianimaz" },
-//       { type: "Linkedin", url: "http://uy.linkedin.com/in/siderys" }
-//     ]
-//   },
-//   {
-//     id: "Fabian Fernandez",
-//     name: "Fabian Fernandez",
-//     imageUrl: "https://i.imgur.com/vXLIy95.jpg",
-//     socialLinks: [
-//       { type: "Twitter", url: "https://twitter.com/kzfabi" },
-//       { type: "Linkedin", url: "http://uy.linkedin.com/in/kzfabi" }
-//     ]
-//   },
-//   {
-//     id: "Pablo Di Loreto",
-//     name: "Pablo Di Loreto",
-//     imageUrl: "http://uy.netconf.global/Content/images/demo/organizers/pablodiloreto.jpg",
-//     socialLinks: [
-//       { type: "Twitter", url: "https://twitter.com/pablodiloreto" },
-//       { type: "Linkedin", url: "http://uy.linkedin.com/in/pablodiloreto" }
-//     ]
-//   },
-//   {
-//     id: "Mariano Vazquez",
-//     name: "Mariano Vazquez",
-//     imageUrl: "http://ar.netconf.global/Content/images/demo/organizers/marianovazquez.jpg",
-//     socialLinks: [
-//       { type: "Twitter", url: "https://twitter.com/nanovazquez87" },
-//       { type: "Linkedin", url: "http://uy.linkedin.com/in/nanovazquez" }
-//     ]
-//   }
-// ];
-
 export default class ExecutiveTeamPage extends React.PureComponent<Props> {
   render() {
     const Resources = resourcesService.getResources();

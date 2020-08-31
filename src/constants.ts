80c9427850572be8f9cd59da1e460d4820a9a629
Facundo La Rocca <facundo_larocca@yahoo.com.ar>
2020-08-30 22:08:23 -0300

    Fix redirect uri

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,12 @@
+const redirectUri = "http%3A%2F%2Fvopen.tech%2Flogin-oidc";
+const clientId = "2bb15913-064f-4bd9-8850-04d5b2a96869";
 const constants = {
   globalSpeakerCallUrl: "https://sessionize.com/vopen-2020/",
   speakerCallUrl: "https://forms.gle/knXD5HcBpYvz4XGDA",
   sponsorsCallUrl: "https://forms.gle/nmMnpHZa3wDytZVbA",
   rsvpUrl: "https://forms.gle/jpackr1z3NoGnpyt7",
   countryRequestUrl: "https://forms.gle/F2cT1dN5EkYYjE6t9",
-  loginUrl: "https://vopentechweb.b2clogin.com/vopentechweb.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_signinsignup&client_id=2bb15913-064f-4bd9-8850-04d5b2a96869&nonce=defaultNonce&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin-oidc&scope=openid&response_type=id_token&prompt=login&response_mode=query"
+  loginUrl: `https://vopentechweb.b2clogin.com/vopentechweb.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_signinsignup&client_id=${clientId}&nonce=defaultNonce&redirect_uri=${redirectUri}&scope=openid&response_type=id_token&prompt=login&response_mode=query`
 };
 
 export default constants;

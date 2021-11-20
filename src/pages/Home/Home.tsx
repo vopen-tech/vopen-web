59d7a48ef737925aca02c6f389006b658ce2027b
Facundo La Rocca <facundo_larocca@yahoo.com.ar>
2021-11-19 23:27:42 -0300

    Property fixed

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -58,8 +58,9 @@ class Home extends React.PureComponent<IProps> {
               height="315" 
               src="https://www.youtube.com/embed/yHhWCmmGCUU" 
               title="YouTube video player" 
+              frameBorder="0" 
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-              allowfullscreen></iframe>
+              allowFullScreen></iframe>
           )
         }
         {

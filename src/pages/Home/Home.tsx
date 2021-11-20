ca6dad07b846cb6bab2ed539203d6a41bc977efb
Facundo La Rocca <facundo_larocca@yahoo.com.ar>
2021-11-19 23:32:49 -0300

    iFrame added (#43)
    * iFrame added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -51,9 +51,23 @@ class Home extends React.PureComponent<IProps> {
 
     return (
       <div className={styles.home}>
-        {siteService.mustSetUpCountDown() && (
-          <CountDown deadline={deadlineDate} current={currentDate} register={() => (window.location.href = Constants.loginUrl)} />
-        )}
+        {
+          (currentDate > deadlineDate) && (
+            <iframe 
+              width="560" 
+              height="315" 
+              src="https://www.youtube.com/embed/yHhWCmmGCUU" 
+              title="YouTube video player" 
+              frameBorder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+              allowFullScreen></iframe>
+          )
+        }
+        {
+          (currentDate <= deadlineDate) && siteService.mustSetUpCountDown() && (
+            <CountDown deadline={deadlineDate} current={currentDate} register={() => (window.location.href = Constants.loginUrl)} />
+          )
+        }
         {/* <CtaButtons className="pv5-l pv4" /> */}
         {/* Tags section */}
         <div className={styles.tags}>
